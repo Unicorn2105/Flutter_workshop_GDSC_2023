13fd05d8bf762b8a80b770d37c71cc629751d12c
Shahd Salah <sh.unicorn2002@gmail.com>
2023-02-04 23:29:48 +0200

    ShahdSalah//final

--- where_map_foreach/for_each.dart
+++ where_map_foreach/for_each.dart
@@ -1,13 +1,17 @@
-
 void main(List<String> args) {
   //! easy
   // print the values of the list using for each method in list
   List<String> names = ['John', 'Jane', 'Jim', 'Jill'];
+  names.forEach((element) {
+    print(element);
+  });
   // output should be John Jane Jim Jill
 
   // Print the index and value of each element in a list of integers:
   List<int> numbers = [1, 2, 3, 4, 5];
-  numbers.forEach((element) {});
+  numbers.forEach((element) {
+    print('${numbers.indexOf(element)}: $element');
+  });
   // output should be
   //0: 1
   //1: 2
@@ -20,13 +24,22 @@ void main(List<String> args) {
   List<String> words = ['Dart', 'Flutter', 'Angular'];
   bool contains = false;
   String search = 'Flutter';
+  words.forEach((element) {
+    if (element == search) {
+      contains = true;
+    }
+  });
   // print true if the list contains the search string
-  
+  print(contains);
+
   //! hard
   // double the values in the map using for each
   Map<String, int> map = {"a": 1, "b": 2, "c": 3, "d": 4};
   Map<String, int> doubledMap = {};
-  map.forEach((key, value) {});
+  map.forEach((key, value) {
+    doubledMap[key] = value * 2;
+  });
+  print(doubledMap);
   // output should be {"a": 2, "b": 4, "c": 6, "d": 8}
 
   // Write a function that takes a List of Maps and returns a Map with the sum of values for each key
@@ -35,18 +48,46 @@ void main(List<String> args) {
     {"a": 2, "b": 4, "c": 6},
     {"a": 3, "b": 6, "c": 9},
   ];
+
   Map<String, int> sumOfValues = {};
-  sumOfValues.forEach((key, value) {});
+  int a = 0, b = 0, c = 0;
+  listOfMaps.forEach((element) {
+    element.forEach((key, value) {
+      if (key == "a") {
+        a += value;
+      }
+      if (key == "b") {
+        b += value;
+      }
+      if (key == "c") {
+        c += value;
+      }
+    });
+  });
+  sumOfValues.addAll({"a": a, "b": b, "c": c});
+  print(sumOfValues);
+
   // output should be {"a": 6, "b": 12, "c": 18}
 
   // Sort a Map by its keys and values
   Map<String, int> alphabets = {'b': 2, 'a': 1, 'c': 3};
   Map<String, int> sortedAlphabets;
+  List<MapEntry<String, int>> entries = alphabets.entries.toList();
+  entries.sort((a, b) => a.key.compareTo(b.key));
+  sortedAlphabets = Map.fromEntries(entries);
+  print(sortedAlphabets);
+
   // output should be {'a': 1, 'b': 2, 'c': 3}
 
   // Filter the map using for each if the value is greater than 3
   Map<String, int> map2 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5};
   Map<String, int> filteredMap = {};
+  map2.forEach((key, value) {
+    if (value > 3) {
+      filteredMap[key] = value;
+    }
+  });
+  print(filteredMap);
   // output should be {'d': 4, 'e': 5}
 
   // Flatten the map
@@ -56,5 +97,11 @@ void main(List<String> args) {
     "person3": {"name": "Jim", "age": 35},
   };
   Map<String, dynamic> result = {};
+  mapOfMaps.forEach((key, value) {
+    value.forEach((key2, value2) {
+      result['$key-$key2'] = value2;
+    });
+  });
+  print(result);
   // Output: {'person1-name': 'John', 'person1-age': 30, 'person2-name': 'Jane', 'person2-age': 25, 'person3-name': 'Jim', 'person3-age': 35}
 }
--- where_map_foreach/map.dart
+++ where_map_foreach/map.dart
@@ -1,7 +1,21 @@
 void main(List<String> args) {
   // use map to convert every grade with A , B , C , D , F
   Map<String, int> grades = {'Ahmed': 75, 'Youssef': 82, 'Sherif': 90};
-  Map<String, String> gradesWithLetters;
+  Map<String, String> gradesWithLetters = {};
+  grades.forEach((key, value) {
+    if (value >= 90) {
+      gradesWithLetters[key] = 'A';
+    } else if (value >= 80) {
+      gradesWithLetters[key] = 'B';
+    } else if (value >= 70) {
+      gradesWithLetters[key] = 'C';
+    } else if (value >= 60) {
+      gradesWithLetters[key] = 'D';
+    } else {
+      gradesWithLetters[key] = 'F';
+    }
+  });
+  print(gradesWithLetters);
   // output should be {'Ahmed': 'C', 'Youssef': 'B', 'Sherif': 'A'}
   
 
